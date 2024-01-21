c260881fb79de5a6925bf39c0a8542f3e83f1138
Linh Truong [9999] <linh.truong@tyme.com>
2024-01-21 15:17:24 +0700

    chore: update

--- dynamodbstream-firehose-s3/lambda-multiple-funcs/src/functionA/main.py
+++ dynamodbstream-firehose-s3/lambda-multiple-funcs/src/functionA/main.py
@@ -1,15 +1,17 @@
 import os
 import uuid
-
+from decimal import Decimal
 from src.shared.repository.dynamodb_repository import put_item
 
 DYNAMO_DB_TABLE = os.environ["DYNAMO_DB_TABLE"]
 def handler(event, context):
     transaction_id = "PARTNER_TXN_ID_pay_2oXYFwvM99Rj9ehNtKgREgrk_" + str(uuid.uuid4())
+    product_code = "MCA#PARTNER_NAME"
     put_item(DYNAMO_DB_TABLE, {
         "PK": "CUSTOMER_TXN#BANK_NAME#MCA#PARTNER_NAME#20240101",
         "SK": transaction_id,
-        "transaction_amount": float(99.99),
+        "product_code": product_code,
+        "transaction_amount": Decimal(str(99.99)),
         "identity_id": "merchant_id_" + str(uuid.uuid4()),  #The unique identifier of the merchant
         "identity_type": "PARTNER_NAME",
         "transaction_id": transaction_id,
