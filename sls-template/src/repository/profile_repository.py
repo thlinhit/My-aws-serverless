e6bf3bc9867155e0a171dfc8b77b45b1aeb18378
Linh Truong [9999] <linh.truong@tyme.com>
2024-10-14 16:37:46 +0700

    use parse

--- sls-template/src/repository/profile_repository.py
+++ sls-template/src/repository/profile_repository.py
@@ -1,6 +1,8 @@
 import os
 from profile import Profile
 
+from aws_lambda_powertools.utilities.parser import parse
+
 from src.repository import dynamodb_repository
 from src.repository.model.key import Key
 from src.repository.model.profile_item import ProfileItem
@@ -10,8 +12,8 @@ table_name = os.environ["DYNAMODB_TABLE"]
 
 def get_profile(profile_id: str) -> Profile:
     key = Key(pk=f"PROFILE#{profile_id}", sk=f"PROFILE#{profile_id}")
-    return ProfileItem(**dynamodb_repository.get_item(table_name=table_name, key=key)).to_dto()
+    return parse(model=ProfileItem, event=dynamodb_repository.get_item(table_name=table_name, key=key)).to_dto()
 
 
 def upsert_profile(profile: Profile) -> Profile:
-    return ProfileItem(**dynamodb_repository.update_item(table_name, ProfileItem.from_dto(profile))).to_dto()
+    return parse(model=ProfileItem, event=dynamodb_repository.update_item(table_name, ProfileItem.from_dto(profile))).to_dto()
