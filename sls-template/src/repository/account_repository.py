ec09708f08acac8ef342cc202d373cd6e1bf44e2
Linh Truong [9999] <linh.truong@tyme.com>
2024-10-14 16:39:05 +0700

    use parse

--- sls-template/src/repository/account_repository.py
+++ sls-template/src/repository/account_repository.py
@@ -1,5 +1,7 @@
 import os
 
+from aws_lambda_powertools.utilities.parser import parse
+
 from src.dto.account import Account
 from src.repository import dynamodb_repository
 from src.repository.model.account_item import AccountItem
@@ -10,8 +12,9 @@ table_name = os.environ["DYNAMODB_TABLE"]
 
 def get_account(account_number: str) -> Account:
     key = Key(pk=f"ACC#{account_number}", sk=f"ACC#{account_number}")
-    return AccountItem(**dynamodb_repository.get_item(table_name=table_name, key=key)).to_dto()
+    return parse(model=AccountItem, event=dynamodb_repository.get_item(table_name=table_name, key=key)).to_dto()
 
 
 def upsert_account(account: Account) -> Account:
-    return AccountItem(**dynamodb_repository.update_item(table_name, AccountItem.from_dto(account))).to_dto()
+    return parse(model=AccountItem,
+                 event=dynamodb_repository.update_item(table_name, AccountItem.from_dto(account))).to_dto()
