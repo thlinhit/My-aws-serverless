37b5925faadebe77bbde3bd09b3ba714345cbccc
Linh <thlinh.it@gmail.com>
2024-10-27 06:15:08 +0700

    feat: add new method to compute hashed id

--- async-command-poc/service/src/controller/dto/place_order_dto.py
+++ async-command-poc/service/src/controller/dto/place_order_dto.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
 
 from src.domain.order import Product, Address, Order
 from src.mapper import generic_mapper
@@ -14,4 +14,7 @@ class PlaceOrderDto(BaseModel):
     address: Address
 
     def to_domain(self) -> Order:
-        return generic_mapper.map(self, Order, extra_fields={"id": str(uuid_util.generate_uuid_from_json(self.model_dump()))})
+        return generic_mapper.map(self, Order, extra_fields={"id": self.compute_hashed_id()})
+
+    def compute_hashed_id(self) -> str:
+        return str(uuid_util.generate_uuid_from_json(self.model_dump()))
