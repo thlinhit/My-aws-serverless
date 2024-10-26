778fa36655c2beb2d9b6e97e20c7c9b095e216b6
Linh <thlinh.it@gmail.com>
2024-10-27 05:45:58 +0700

    chore: add json util

---
+++ async-command-poc/service/src/util/json_util.py
@@ -0,0 +1,26 @@
+import json
+from decimal import Decimal
+from types import SimpleNamespace
+from typing import Any
+
+
+def dumps(json_body: dict, **kwargs) -> str:
+    def default_serializer(obj: Any) -> Any:
+        if isinstance(obj, Decimal):
+            return float(obj)
+        raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
+
+    return json.dumps(
+        json_body,
+        default=default_serializer,
+        **kwargs
+    )
+
+
+def loads(json_str: str, **kwargs) -> dict:
+    return json.loads(
+        json_str,
+        object_hook=lambda d: SimpleNamespace(**d),
+        parse_float=Decimal,
+        **kwargs
+    )
