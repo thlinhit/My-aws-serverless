2d4e95dc54b0056999dd8aeefdec2546f87c02bd
Linh Truong [9999] <linh.truong@tyme.com>
2024-11-04 10:37:01 +0700

    refactor(ssm-provider): add utility method

--- async-command-poc/order-service/src/config/aws_ssm_config.py
+++ async-command-poc/order-service/src/config/aws_ssm_config.py
@@ -4,6 +4,7 @@ from aws_lambda_powertools.utilities import parameters
 from botocore.config import Config
 
 from src.config.env import REGION
+from src.log.logger import logger
 
 
 @cache
@@ -14,3 +15,7 @@ def get_ssm_provider():
         retries={"total_max_attempts": 2, "max_attempts": 5}
     )
     return parameters.SSMProvider(config=config)
+
+def get(ssm_key: str, max_age: int = 900) -> str:
+    logger.debug(f"Fetching SSM value for key: {ssm_key}")
+    return get_ssm_provider().get(ssm_key, max_age)
